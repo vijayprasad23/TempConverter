94ea1de4f7318baab402fa9abdcef201af1eec4c
William Purdon <ac117097@avcol.school.nz>
2022-03-07 12:16:54 +1300

    Added variables for C and F and added table.

--- TempConverter/Program.cs
+++ TempConverter/Program.cs
@@ -6,8 +6,10 @@ namespace TempConverter
     {
         static void Main(string[] args)
         {
+            int celcius, fahrenheit;
             Console.WriteLine("This program will ask the user to enter the temperature in degree celsius and it will convert it to degrees fahrenheit");
-
+            Console.WriteLine("Celcius       Fahrenheit");
+            Console.WriteLine("=======       ==========");
             //
         }
     }
