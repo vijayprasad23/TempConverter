ca4fae8278d0028e38db08198df4ce87b2dae2d9
Vijay Prasad <psv@avcol.school.nz>
2022-03-07 11:41:40 +1300

    Add project files.

---
+++ TempConverter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TempConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program will ask the user to enter the temperature in degree celsius and it will convert it to degrees fahrenheit");
+
+            //
+        }
+    }
+}
